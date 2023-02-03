8e43caa0517bc3a681be75e8b8ca6ed63e9d1f78
thisiskritesh <kritesht43@gmail.com>
2023-02-03 21:10:07 +0530

    added syntax

---
+++ syntax.py
@@ -0,0 +1,13 @@
+if 5 > 2:
+    print("heyo  nigga!!")
+
+print("haa bhai aa gaya bahar no error")
+
+if 1 > 2:
+    print("nub")
+else:
+    print("pro")
+
+if 5 > 2:
+    print("pro") 
+    print("nub")  
